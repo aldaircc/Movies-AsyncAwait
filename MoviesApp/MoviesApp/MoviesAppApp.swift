46b127524223e116c77b9079761752fd74c19f70
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2023-08-16 19:07:58 -0500

    base files

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Aldair Cosetito Coral on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
